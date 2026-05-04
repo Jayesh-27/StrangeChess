b801a52ddc0c6db8e3f654504094b3e28ecef471
Jayesh27 <jayeshgfatnani27@gmail.com>
2026-05-04 20:29:35 +0530

    Fixed Merge Conflicts

--- StrangeChess/Assets/Board.cs
+++ StrangeChess/Assets/Board.cs
@@ -2,70 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum pieceType
+{
+    none,
+    whitePawn,
+    whiteKnight,
+    whiteBishop,
+    whiteRook,
+    whiteQueen,
+    whiteKing,
+    blackPawn,
+    blackKnight,
+    blackBishop,
+    blackRook,
+    blackQueen,
+    blackKing
+}
+
 public class Board : MonoBehaviour
 {
-    [SerializeField] public ulong whitePawn   = 0x00FF000000000000;
-    [SerializeField] public ulong whiteRook   = 0x8100000000000000;
-    [SerializeField] public ulong whiteKnight = 0x4200000000000000;
-    [SerializeField] public ulong whiteBishop = 0x2400000000000000;
-    [SerializeField] public ulong whiteQueen  = 0x1000000000000000;
-    [SerializeField] public ulong whiteKing   = 0x0800000000000000;
-                      
-    [SerializeField] public ulong blackPawn   = 0x000000000000FF00;
-    [SerializeField] public ulong blackRook   = 0x0000000000000081;
-    [SerializeField] public ulong blackKnight = 0x0000000000000042;
-    [SerializeField] public ulong blackBishop = 0x0000000000000024;
-    [SerializeField] public ulong blackQueen  = 0x0000000000000008;
-    [SerializeField] public ulong blackKing   = 0x0000000000000010;
-                      
+    [SerializeField] public ulong[] pieceBitboards = new ulong[13];
+
     [SerializeField] public ulong whitePieces;
     [SerializeField] public ulong blackPieces;
     [SerializeField] public ulong allPieces;
     [SerializeField] public ulong[] knightAttacks = new ulong[64];
 
-    [SerializeField] public ulong fileB = 0x00FF000000000000;
+    // Rank 2 mask for LERF indexing (A1 = bit 0).
+    [SerializeField] public ulong fileB = 0x000000000000FF00;
 
-    public static Board board;
+    public static Board Instance;
 
     void Awake()
     {
-        if(Board.board == null)
+        if (Instance == null)
         {
-<<<<<<< Updated upstream
-            board = this;
-        }        
-=======
             Instance = this;
         }
 
-        pieceBitboards[(int)pieceType.whitePawn] = 0x00FF000000000000;
-        pieceBitboards[(int)pieceType.whiteKnight] = 0x4200000000000000;
-        pieceBitboards[(int)pieceType.whiteBishop] = 0x2400000000000000;
-        pieceBitboards[(int)pieceType.whiteRook] = 0x8100000000000000;
-        pieceBitboards[(int)pieceType.whiteQueen] = 0x1000000000000000;
-        pieceBitboards[(int)pieceType.whiteKing] = 0x0800000000000000;
-        pieceBitboards[(int)pieceType.blackPawn] = 0x000000000000FF00;
-        pieceBitboards[(int)pieceType.blackKnight] = 0x0000000000000042;
-        pieceBitboards[(int)pieceType.blackBishop] = 0x0000000000000024;
-        pieceBitboards[(int)pieceType.blackRook] = 0x0000000000000081;
-        pieceBitboards[(int)pieceType.blackQueen] = 0x0000000000000008;
-        pieceBitboards[(int)pieceType.blackKing] = 0x0000000000000010;
->>>>>>> Stashed changes
-    }
-
-    void Start()
-    {
-<<<<<<< Updated upstream
-        Debug.Log(displayBitboard(whitePawn));        
-=======
-        calculateKnightAttacks();
-        Debug.Log(knightAttacks[0]);
-        Debug.Log(knightAttacks[63]);
->>>>>>> Stashed changes
-    }
-    void Update()
-    {
+        InitializeBitboards();
         CalculateExtraBitboards();
+        CalculateKnightAttacks();
     }
 
     public string displayBitboard(ulong bitboard)
@@ -73,35 +50,65 @@ public class Board : MonoBehaviour
         return System.Convert.ToString((long)bitboard, 2).PadLeft(64, '0');
     }
 
-    void CalculateExtraBitboards()
+    public void CalculateExtraBitboards()
     {
-        whitePieces = whitePawn | whiteKnight | whiteBishop | whiteRook |whiteQueen | whiteKing;
-        blackPieces = blackPawn | blackKnight | blackBishop | blackRook |blackQueen | blackKing;
+        whitePieces = pieceBitboards[(int)pieceType.whitePawn] |
+                      pieceBitboards[(int)pieceType.whiteKnight] |
+                      pieceBitboards[(int)pieceType.whiteBishop] |
+                      pieceBitboards[(int)pieceType.whiteRook] |
+                      pieceBitboards[(int)pieceType.whiteQueen] |
+                      pieceBitboards[(int)pieceType.whiteKing];
+
+        blackPieces = pieceBitboards[(int)pieceType.blackPawn] |
+                      pieceBitboards[(int)pieceType.blackKnight] |
+                      pieceBitboards[(int)pieceType.blackBishop] |
+                      pieceBitboards[(int)pieceType.blackRook] |
+                      pieceBitboards[(int)pieceType.blackQueen] |
+                      pieceBitboards[(int)pieceType.blackKing];
+
         allPieces = whitePieces | blackPieces;
     }
-<<<<<<< Updated upstream
-}
-=======
 
     public pieceType bitboardToPiece(ulong from)
     {
-        if ((from & allPieces) == 0) return pieceType.none;
-        
+        if ((from & allPieces) == 0)
+        {
+            return pieceType.none;
+        }
+
         for (int i = 1; i <= 12; i++)
         {
-            if ((from & pieceBitboards[i]) != 0) 
+            if ((from & pieceBitboards[i]) != 0)
             {
                 return (pieceType)i;
             }
         }
-        
+
         return pieceType.none;
     }
-    private void calculateKnightAttacks()
+
+    void InitializeBitboards()
+    {
+        pieceBitboards[(int)pieceType.whitePawn] = 0x000000000000FF00;
+        pieceBitboards[(int)pieceType.whiteKnight] = 0x0000000000000042;
+        pieceBitboards[(int)pieceType.whiteBishop] = 0x0000000000000024;
+        pieceBitboards[(int)pieceType.whiteRook] = 0x0000000000000081;
+        pieceBitboards[(int)pieceType.whiteQueen] = 0x0000000000000008;
+        pieceBitboards[(int)pieceType.whiteKing] = 0x0000000000000010;
+        pieceBitboards[(int)pieceType.blackPawn] = 0x00FF000000000000;
+        pieceBitboards[(int)pieceType.blackKnight] = 0x4200000000000000;
+        pieceBitboards[(int)pieceType.blackBishop] = 0x2400000000000000;
+        pieceBitboards[(int)pieceType.blackRook] = 0x8100000000000000;
+        pieceBitboards[(int)pieceType.blackQueen] = 0x1000000000000000;
+        pieceBitboards[(int)pieceType.blackKing] = 0x0800000000000000;
+    }
+
+    void CalculateKnightAttacks()
     {
         int[,] offsets = {
             { 2, 1 }, { 2, -1 }, { -2, 1 }, { -2, -1 },
-            { 1, 2 }, { 1, -2 }, { -1, 2 }, { -1, -2 }};
+            { 1, 2 }, { 1, -2 }, { -1, 2 }, { -1, -2 }
+        };
 
         for (int sq = 0; sq < 64; sq++)
         {
@@ -115,16 +122,14 @@ public class Board : MonoBehaviour
                 int newFile = file + offsets[i, 0];
                 int newRank = rank + offsets[i, 1];
 
-                if (newFile >= 0 && newFile < 8 &&
-                    newRank >= 0 && newRank < 8)
+                if (newFile >= 0 && newFile < 8 && newRank >= 0 && newRank < 8)
                 {
                     int targetIndex = newRank * 8 + newFile;
                     moves |= 1UL << targetIndex;
                 }
             }
 
-            knightAttacks[63 - sq] = moves;
+            knightAttacks[sq] = moves;
         }
     }
 }
->>>>>>> Stashed changes
--- StrangeChess/Assets/Chess.cs
+++ StrangeChess/Assets/Chess.cs
@@ -4,56 +4,32 @@ using UnityEngine;
 
 public class Chess : MonoBehaviour
 {
-    public static Chess chess;
+    public static Chess Instance;
 
     private void Awake()
     {
-        if (chess == null)
+        if (Instance == null)
         {
-            chess = this;
+            Instance = this;
         }
     }
     public void pawnMoves(ulong from)
     {
-        // TODO - Promotion
-        // TODO - En passant
-        // DONE - Single Move
-        // DONE - Double Move
-        // TODO - Captures
-
-        ulong captures = from >> 7 | from >> 9;
-        if((captures & Board.board.blackPieces) != 0)
+        ulong captures = from << 7 | from << 9;
+        
+        ulong whiteCaptures = ((from << 7) & ~0x0101010101010101UL) | ((from << 9) & ~0x8080808080808080UL);
+        if((whiteCaptures & Board.Instance.blackPieces) != 0)
         {
-            ClickDetector.clickDetector.availableMoves = ClickDetector.clickDetector.availableMoves | (captures & Board.board.blackPieces);
+            ClickDetector.Instance.availableMoves |= (whiteCaptures & Board.Instance.blackPieces);
         }
-<<<<<<< Updated upstream
-        Debug.Log("From >> 8" + Board.board.displayBitboard(from >> 8));
-        if(((from >> 8) & Board.board.allPieces) == 0)   // square is empty
+        
+        if(((from << 8) & Board.Instance.allPieces) == 0)   // square is empty
         {
-            Debug.Log("Pawn can move 1 Square");
-            ClickDetector.clickDetector.availableMoves = ClickDetector.clickDetector.availableMoves | from >> 8;
-            Debug.Log("From >> 8" + Board.board.displayBitboard(16));
-            if((from & Board.board.fileB) != 0 && ((from >> 16) & Board.board.allPieces) == 0)
+            ClickDetector.Instance.availableMoves |= from << 8;
+            if((from & Board.Instance.fileB) != 0 && ((from << 16) & Board.Instance.allPieces) == 0)
             {
-                Debug.Log("Pawn can move 2 Square");
-                ClickDetector.clickDetector.availableMoves = ClickDetector.clickDetector.availableMoves | from >> 16;
-=======
-        //Debug.Log("From >> 8" + Board.Instance.displayBitboard(from >> 8));
-        if(((from >> 8) & Board.Instance.allPieces) == 0)   // square is empty
-        {
-            //Debug.Log("Pawn can move 1 Square");
-            ClickDetector.Instance.availableMoves = ClickDetector.Instance.availableMoves | from >> 8;
-            //Debug.Log("From >> 8" + Board.Instance.displayBitboard(16));
-            if((from & Board.Instance.fileB) != 0 && ((from >> 16) & Board.Instance.allPieces) == 0)
-            {
-                //Debug.Log("Pawn can move 2 Square");
-                ClickDetector.Instance.availableMoves = ClickDetector.Instance.availableMoves | from >> 16;
->>>>>>> Stashed changes
+                ClickDetector.Instance.availableMoves |= from << 16;
             }
-        }
-        else
-        {
-            Debug.Log("pawn can not move");
         }
     }
 
@@ -64,59 +40,21 @@ public class Chess : MonoBehaviour
 
     public void movePiece(ulong from, ulong to)
     {
-        if ((from & Board.board.whitePieces) != 0)
+        pieceType piece = Board.Instance.bitboardToPiece(from);
+
+        if(piece == pieceType.none)
+            return;
+            
+        if(piece < pieceType.blackPawn)
         {
-            if((to & Board.board.blackPieces) != 0)
+            pieceType toPiece = Board.Instance.bitboardToPiece(to);
+            if(toPiece >= pieceType.blackPawn)
             {
-                if ((to & Board.board.blackPawn) != 0)
-            {
-                Board.board.blackPawn = Board.board.blackPawn ^ to;
+                Board.Instance.pieceBitboards[(int)toPiece] ^= to;
             }
-            else if ((to & Board.board.blackKnight) != 0)
-            {
-                Board.board.blackKnight = Board.board.blackKnight ^ to;
-            }
-            else if ((to & Board.board.blackBishop) != 0)
-            {
-                Board.board.blackBishop = Board.board.blackBishop ^ to;
-            }
-            else if ((to & Board.board.blackRook) != 0)
-            {
-                Board.board.blackRook = Board.board.blackRook ^ to;
-            }
-            else if ((to & Board.board.blackQueen) != 0)
-            {
-                Board.board.blackQueen = Board.board.blackQueen ^ to;
-            }
-            else if ((to & Board.board.blackKing) != 0)
-            {
-                Board.board.blackKing = Board.board.blackKing ^ to;
-            }
-            }
-            if ((from & Board.board.whitePawn) != 0)
-            {
-                Board.board.whitePawn = (Board.board.whitePawn ^ from) | to;
-            }
-            else if ((from & Board.board.whiteKnight) != 0)
-            {
-                Board.board.whiteKnight = (Board.board.whiteKnight ^ from) | to;
-            }
-            else if ((from & Board.board.whiteBishop) != 0)
-            {
-                Board.board.whiteBishop = (Board.board.whiteBishop ^ from) | to;
-            }
-            else if ((from & Board.board.whiteRook) != 0)
-            {
-                Board.board.whiteRook = (Board.board.whiteRook ^ from) | to;
-            }
-            else if ((from & Board.board.whiteQueen) != 0)
-            {
-                Board.board.whiteQueen = (Board.board.whiteQueen ^ from) | to;
-            }
-            else if ((from & Board.board.whiteKing) != 0)
-            {
-                Board.board.whiteKing = (Board.board.whiteKing ^ from) | to;
-            }
+            Board.Instance.pieceBitboards[(int)piece] = (Board.Instance.pieceBitboards[(int)piece] ^ from) | to;
         }
+        Board.Instance.CalculateExtraBitboards();
     }
 }
+
